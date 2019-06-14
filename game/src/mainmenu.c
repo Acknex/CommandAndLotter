1975b27941abffba1399b30a824249f1a0b92cc3
Felix Queißner <git@mq32.de>
2019-06-14 22:51:56 +0200

    Adds awesome main menu to enter different substates

--- game/src/mainmenu.c
+++ game/src/mainmenu.c
@@ -13,14 +13,25 @@ void mainmenu_open(void)
 
 void mainmenu_update(void)
 {
-
+	draw_text(
+		"1: start game\r\n2: credits\r\n3: exit",
+		16,  16,
+		COLOR_RED
+	);
 }
 
 void mainmenu_close(void)
 {
+
 }
 
 int mainmenu_get_response()
 {
+	if(key_1)
+		return MAINMENU_RESPONSE_STARTGAME;
+	if(key_2)
+		return MAINMENU_RESPONSE_CREDIT;
+	if(key_3)
+		return MAINMENU_RESPONSE_EXIT;
 	return MAINMENU_RESPONSE_KEEP;
 }
