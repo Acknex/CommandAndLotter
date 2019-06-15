277ac967e35244076d09bfe0ba37a4569b8a0200
Steffen Pohl <steffen.pohl@gmx.de>
2019-06-15 21:33:56 +0200

    FOV: externe LOS Funktion

--- game/src/fow.c
+++ game/src/fow.c
@@ -49,14 +49,37 @@ void fow_open()
 		for(y = 0; y<map->size[1]; ++y)
 		{
 			TILE *tile = mapTileGet(map, x,y);
-			tile->visibility = FOW_HIDDEN;
 			
 			VECTOR pos;
 			mapGetVectorFromTile(map, &pos, tile);
 			pos.z = 500;
+			
+			
+			tile->visibility = FOW_HIDDEN;
             effect(Fog, 1, pos, nullvector);
 		}
 #endif
+}
+
+
+void fov_uncover(VECTOR *pos, var range)
+{
+	MAP *map = mapGetCurrent();
+	TILE *tile = mapGetTileFromVector(map, pos);
+	
+	int iRange = floor(range / map->tileSize + 0.5);
+	
+	int i,j;
+	for(i = tile->pos[0]-iRange; i <= tile->pos[0]+iRange; i++)
+		for(j = tile->pos[1]-iRange; j <= tile->pos[1]+iRange; j++)
+		{
+			TILE *otherTile = mapTileGet(map, i, j);
+			VECTOR otherPos;
+			apGetVectorFromTile(map, &otherPos, otherTile)
+			
+			if(vec_dist(pos, otherPos) < range)
+				otherTile->visibility = FOW_SCOUTED;
+		}
 }
 
 void fow_update()
@@ -71,7 +94,6 @@ void fow_update()
 			TILE *tile = mapTileGet(map, x,y);
 			if(tile->visibility == FOW_HIDDEN){
 				if(mapIsAnyFriendlyUnitNearby(map, tile, FOW_SIGHT_RANGE, PLAYER_ID_PLAYER)) 
-				//if(mapIsAnyUnitNearby(map, tile, FOW_SIGHT_RANGE)) 
 				{
 					tile->visibility = FOW_SCOUTED;	
 				}
