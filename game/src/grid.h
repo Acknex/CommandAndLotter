b7a29d3d417294e7fbdcfd0e200e59ca9b5c4e4d
Steffen Pohl <steffen.pohl@gmx.de>
2019-06-15 15:26:04 +0200

    Gridfunktionalität

--- game/src/grid.h
+++ game/src/grid.h
@@ -1,7 +1,7 @@
 #ifndef GRID_H
 #define GRID_H
 
-#define GRID_CELLSIZE 100
+#define GRID_CELLSIZE 200
 
 //bitmask
 #define GRID_BLOCKED 1
@@ -11,18 +11,20 @@ void grid_init();
 void grid_open(int xSize, int ySize);
 void grid_close();
 
+
 int grid_CoordToID(int x, int y);
 
 void grid_setState(int x, int y, int state);
 void grid_resetState(int x, int y, int state);
 int grid_getState(int x, int y);
 
-
 void grid_setState_byID(int id, state);
 void grid_resetState_byID(int id, state);
 int grid_getState_byID(int id);
 
-int grid_getCellID_byPos(VECTOR);
-VECTOR grid_getCellPos_byID(int);
+
+int grid_getCellID(VECTOR);
+VECTOR *grid_getCellCenter(int x, int y);
+VECTOR *grid_getCellCenter_byID(int);
 
 #endif
