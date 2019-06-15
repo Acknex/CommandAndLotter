f8781b9da12a991def59b7bd9d8384de287291b9
Patrick Jendral <pj@gassmann.it>
2019-06-15 21:43:38 +0200

    ui_game_after_all

--- game/src/ui_game.h
+++ game/src/ui_game.h
@@ -58,6 +58,7 @@ BMAP *ui_hide_button_p = "hide_button_p.png";
 
 BMAP *ui_radial_o = "ui_placeholder_counter.png";
 BMAP *ui_radial_n = "ui_placeholder_unit.png";
+BMAP *ui_radial_progress = "#116x116x8888";
 
 BMAP *ui_icon_cbabe = "ui_H2.png";
 BMAP *ui_icon_sputnik = "ui_H1.png";
