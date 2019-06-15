1beb12c8c21c930910fed5c8b4da2b4e1ef4e68f
Steffen Pohl <steffen.pohl@gmx.de>
2019-06-15 13:34:23 +0200

    Kameramittelpunkt kommt mit Rotation klar

--- game/src/camera.c
+++ game/src/camera.c
@@ -12,6 +12,9 @@ void topdown_camera_open()
 
 void topdown_camera_update() 
 {
+	if(key_p)
+		topdown_camera_set_pos(nullvector);
+	
 #ifdef DEBUG
 	if(def_camera == 0)
 #endif
@@ -26,6 +29,7 @@ void topdown_camera_update()
 	
 		topdown_camera_center.x += (key_updown*cosv(camera.pan)- key_leftright*sinv(camera.pan))*time_step*CAMERA_SPEED;
 		topdown_camera_center.y += (key_leftright*cosv(camera.pan)+ key_updown*sinv(camera.pan))*time_step*CAMERA_SPEED;
+		topdown_camera_center.z = 0;
 		
 		topdown_camera_height += -0.1*(topdown_camera_height/10)*mickey.z*time_step;
 		topdown_camera_height = clamp(topdown_camera_height, 1, 10.);
@@ -34,7 +38,9 @@ void topdown_camera_update()
 		VECTOR offset;
 		vec_set(&offset, nullvector);
 		offset.z = -sinv(camera.tilt)*CAMERA_HEIGHT*topdown_camera_height;
-		offset.x = -cosv(camera.tilt)*CAMERA_HEIGHT*topdown_camera_height;
+		
+		offset.x = -cosv(camera.tilt)*CAMERA_HEIGHT*topdown_camera_height*cosv(camera.pan);
+		offset.y = -cosv(camera.tilt)*CAMERA_HEIGHT*topdown_camera_height*sinv(camera.pan);
 		
 		vec_set(&camera.x, vec_add(offset, topdown_camera_center));
 	}
