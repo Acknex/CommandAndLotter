c9dbc712c30305a444b56ccb87c0b40288c8f66e
Philipp Meißner <p.meissner@digital-nw.de>
2019-06-15 20:15:05 +0200

    Phoque

--- game/src/fow.c
+++ game/src/fow.c
@@ -4,30 +4,39 @@
 
 
 BMAP* FogBmap = "FogOfWar.tga";
-
+VECTOR MousePosFog;
+var FogMove;
 
 void FogEvent(PARTICLE *p)
 {
     p.lifespan = 2;
+    p.x = p.skill_a + FogMove*100+(p.skill_a%100);
+
+    if(vec_dist(vector(p.skill_a, p.skill_b, 500),MousePosFog) < 150){
+        p.lifespan = 0;
+    }
 }
 
 void Fog(PARTICLE *p)
 {
     p.bmap = FogBmap;
-    p.alpha = 100;
+    p.alpha = 90;
     p.gravity = 0;
     p.size = 100;
 
     p.flags |= (MOVE | TRANSLUCENT);
     p.event = FogEvent;
+    p.skill_a = p.x;
+    p.skill_b = p.y;
 }
 
 
 void FogOfWarTest()
 {
+    max_particles = 1000000;
     var x, y;
     for(x = -1000; x < 1000; x+=100){
-        for(y = -1000; y < 10000; y+=100){
+        for(y = -1000; y < 1000; y+=100){
             effect(Fog, 1, vector(x,y,500), nullvector);
         }
     }
@@ -75,4 +84,9 @@ void fow_update()
 						tile->visibility = FOW_SCOUTED;	
 			}
 	}
+
+
+    PosToMap(MousePosFog,mouse_pos.x,mouse_pos.y);
+    MousePosFog.z = 500;
+    FogMove = sinv(total_ticks);
 }
